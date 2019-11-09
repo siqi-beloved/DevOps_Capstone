2d67746ebbfd1af73a7de03b19deaa223e0a4f88
Siqi Li <55540091+siqi-beloved@users.noreply.github.com>
2019-11-09 12:44:02 -0800

    f8s file update

--- Deploy_kubenetes.sh
+++ Deploy_kubenetes.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-echo "Creating the volume..."
-
-kubectl apply -f ./kubernetes/persistent-volume.yaml
-kubectl apply -f ./kubernetes/persistent-volume-claim.yaml
-
-
-echo "Creating the database credentials..."
-
-kubectl apply -f ./kubernetes/postgres-secret.yaml
-
-
-echo "Creating the postgres deployment and service..."
-
-kubectl create -f ./kubernetes/postgres-deployment.yaml
-kubectl create -f ./kubernetes/postgres-service.yaml
-
-
 echo "Creating the flask deployment and service..."
 
 kubectl create -f ./kubernetes/flask-deployment.yaml
