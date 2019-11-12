84ef203c236fec4089178b6de1b2c3cf4831f4b2
Siqi Li <55540091+siqi-beloved@users.noreply.github.com>
2019-11-12 11:46:54 -0800

    Add deploy to cluster script

---
+++ deploy to cluster.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#get nodes status
+kubectl get nodes
+#pull image from dockerhub
+docker pull siqili/capstone:0.1
+#create deplyments
+kubectl create deployment capstone --image=siqili/capstone:0.1
+#get deployment status
+Kbectl get deployments
